ed877325160ace1677bdc72703a110f10ab8d467
JohnnyMCR <jmcastillo991@gmail.com>
2022-12-13 20:02:32 -0500

    eventsLabDone

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const painting = document.querySelector('#palette').style.background
+document.querySelectorAll('#palette').forEach(div => {
+  div.addEventListener('click', pickColor => {
+    document.querySelector('#current-color').style.background = pickColor.target.style.background;
+  })
+});
+
+document.querySelectorAll('.cell').forEach(cell => {
+  cell.addEventListener('click', section => {
+    section.target.style.background = document.querySelector('#current-color').style.background;
+  })
+});
